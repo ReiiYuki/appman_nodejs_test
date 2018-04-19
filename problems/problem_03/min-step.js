2b70b751afc60b51409734fd5adbddd96037c021
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-04-19 14:37:04 +0700

    Minstep

--- problems/problem_03/min-step.js
+++ problems/problem_03/min-step.js
@@ -1,5 +1,17 @@
 const minStep = input => {
-  // put your code here !!
-  return 0;
+  if (input%2==0) {
+    input /= 2
+    if (input > 1) return 1 + minStep(input)
+    else return 1
+  } else {
+    let n1 = input+1
+    let n2 = input-1
+    const result1 = minStep(n1)
+    const result2 = minStep(n2)
+    if (result1 < result2) return 1 + result1
+    else return 1 + result2
+  }
 };
+
 module.exports = { minStep };
+
