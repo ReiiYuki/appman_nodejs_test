d149d3fbf16c106b99de6093fa83138be0c8e54a
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-04-19 14:53:09 +0700

    Problem 4

--- problems/problem_04/crypto-details.js
+++ problems/problem_04/crypto-details.js
@@ -1,5 +1,15 @@
-const cryptoDetails = async (symbols = []) => {
-  // put your code here !!
-};
+const request = require('request')
 
+const cryptoDetails = async (symbols = []) => {
+  let responses = []
+  for (let i = 0;i<symbols.length;i++) {
+    const response = await new Promise((resolve, reject) => { request(`https://coinbin.org/${symbols[i]}`, (err, res, body) => {
+      if (err) reject(err)
+      else resolve(JSON.parse(body))
+    }) })
+    responses.push(response)
+  }
+  let result = await Promise.all(responses)
+  return result
+};
 module.exports = { cryptoDetails };
