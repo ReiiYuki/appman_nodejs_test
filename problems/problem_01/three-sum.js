ce39c17fb20ca72ae82da8cc2adf5aa8e13cce99
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-04-19 15:55:54 +0700

    Problem 1

--- problems/problem_01/three-sum.js
+++ problems/problem_01/three-sum.js
@@ -1,6 +1,15 @@
-const threeSum = (nums, target) => {
-  // put your code here !!
-  return [];
-};
 
+const threeSum = (nums, target) => {
+  let result = []
+  for (let i = 0;i < nums.length; i++)
+    for (let j = 0; j < nums.length; j++)
+      if (i!==j)
+        for (let k = 0; k<nums.length; k++)
+          if (i!==j && k!==j && i!==k)
+            if (nums[i]+nums[j]+nums[k] === target)
+              if (!result.includes([i,j,k].sort())){
+                result.push([i,j,k].sort())
+              }
+  return Array.from(new Set(result.map(JSON.stringify)), JSON.parse)
+};
 module.exports = { threeSum };
