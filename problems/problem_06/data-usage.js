4d3483da9eb988d498df172a9b06f826553f33cf
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-04-19 15:42:01 +0700

    Problem 6

--- problems/problem_06/data-usage.js
+++ problems/problem_06/data-usage.js
@@ -3,8 +3,20 @@ const logFilePath = __dirname + '/data-usage.log';
 
 const raw = readFileSync(logFilePath, 'utf8');
 
-const dataUsage = () => {
-  // put your code here !!
-};
+const dataUsage = () => new Promise((resolve,reject) => {
+  let data = {}
+  raw.split('\n').forEach((datum) => {
+    const extractedDatum = datum.split(',')
+    const user = extractedDatum[1].split('user=')[1]
+    const dat = Number(extractedDatum[2].split('data=')[1])
+    data[user] = {
+      total: data[user]? data[user].total+Number(dat):Number(dat),
+      num: data[user]? data[user].num+1:1 
+    }
+  })
 
+  const val = []
+  Object.keys(data).forEach(dat => val.push({ user: dat, total: data[dat].num, average: (data[dat].num/data[dat].total) }))
+  resolve(val)
+});
 module.exports = { dataUsage };
