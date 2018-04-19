d00793cbfa5f59169b622062f1909387ec46b67b
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-04-19 15:13:40 +0700

    Problem 5

--- problems/problem_05/bangkok-forecast.js
+++ problems/problem_05/bangkok-forecast.js
@@ -1,5 +1,17 @@
-const bangkokForecast = async () => {
-  // put your code here !!
-};
 
+const request = require('request')
+
+const bangkokForecast = async () => {
+  return new Promise((resolve, reject) => request(`https://api.openweathermap.org/data/2.5/forecast/daily?q=Bangkok&cnt=7&appid=e5446373eef6128679c7fa8a1951d788`, (err, res, body) => {
+    if (err) reject(err)
+    else resolve(JSON.parse(body).list.map(data => {
+      const date = new Date(data.dt*1000)
+      return {
+        date: `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`,
+        minTemp: data.temp.min,
+        maxTemp: data.temp.max
+      }
+    }))
+  }))
+};
 module.exports = { bangkokForecast };
